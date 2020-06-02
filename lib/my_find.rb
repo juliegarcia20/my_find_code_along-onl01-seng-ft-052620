7fa6cc5c936498c7c2751382a25aa49bd7cff90f
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-02 19:31:01 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,6 +3,10 @@ require 'pry'
 def my_find(collection)
   index = 0 
   
-  while inde
+  while index < collection.length 
 
+
+return collection[index] if yield(collection[index])
+index += 1
+ end 
 end
